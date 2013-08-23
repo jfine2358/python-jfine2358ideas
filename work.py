d43ef543b43e9067b96a28d684fee870d8327752
Jonathan Fine <jfine2358@gmail.com>
2013-08-23 22:24:05 +0100

    Add proof-of-concept argument processing.

--- work.py
+++ work.py
@@ -8,9 +8,15 @@ class Base:
 
     def __init__(self, *argv, **kwargs):
 
+        argv, kwargs = self.process_args(*argv, **kwargs)
         self.argv = argv
         self.kwargs = kwargs
         self.body = None
+
+
+    @staticmethod
+    def process_args(*argv, **kwargs):
+        return argv, kwargs
 
 
     def __getitem__(self, body):
@@ -26,15 +32,19 @@ def wibble(cls):
 
     d = dict(cls.__dict__)
     name = cls.__name__
-    new_cls = meta(name, (Base,), {})
+    new_cls = meta(name, (Base,), d)
     return new_cls
 
 
 @wibble
 class apple:
-    pass
 
-x = apple(x=4, b=5)[
+    @staticmethod
+    def process_args(a=1, b=2, x=3):
+        return (), dict(a=a, b=b, x=x)
+
+
+x = apple(3, 4, x=5)[
     'abc',
 ]
 
