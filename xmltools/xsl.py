2e16e60f9d486dd1b5efd827e70616c12ff7419b
Jonathan Fine <jfine2358@gmail.com>
2013-08-24 10:59:51 +0100

    Add stub xsl.py.

---
+++ xmltools/xsl.py
@@ -0,0 +1,13 @@
+'''XLS elements
+
+'''
+
+__metaclass__ = type
+from .core import elementclass
+
+NAMESPACE = 'http://www.w3.org/1999/XSL/Transform'
+
+if __name__ == '__main__':
+
+    import doctest
+    print(doctest.testmod())
