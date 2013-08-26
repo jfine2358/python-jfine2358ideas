ec0e73f17f9c590c41cae6ff665764f01f762bd5
Jonathan Fine <jfine2358@gmail.com>
2013-08-26 21:32:43 +0100

    Make a start on tagtree.xsl (the name of a Python module for XSL).

---
+++ tagtree/xsl.py
@@ -0,0 +1,42 @@
+'''Tags for xslt stylesheets
+
+>>> def doit(tree): print(tree.pp_xml[:-1])
+>>> doit(apply_templates())
+<apply_templates/>
+
+
+TODO: Add xml etc to the metaclass to avoid
+
+>>> doit(apply_templates)
+Traceback (most recent call last):
+TypeError: 'property' object is unsubscriptable
+
+'''
+
+__metaclass__ = type
+
+
+from .core import tagdecoratorfactory
+from .core import wobble        # TODO: rename, refactor.
+
+
+class XslBase:
+    pass
+
+
+xsltag = tagdecoratorfactory((XslBase, wobble))
+
+
+@xsltag
+def apply_templates(select=None, mode=None):
+    '''
+    TODO: This is not being picked up by doctest.
+    >>> aaa
+    '''
+
+    return locals()
+
+if __name__ == '__main__':
+
+    import doctest
+    print(doctest.testmod())
