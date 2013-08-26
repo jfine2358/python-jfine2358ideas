1a4d629c4f262a53b730e0cf2e09786030d4d2eb
Jonathan Fine <jfine2358@gmail.com>
2013-08-26 17:39:16 +0100

    New-style: able to define classes, instances, and mutate via tag[...].

--- xmltools/core2.py
+++ xmltools/core2.py
@@ -2,9 +2,60 @@
 
 '''
 
-import lxml.etree
+# import lxml.etree
 
 __metaclass__ = type
+
+# DONE: Able to create classes and instances.
+# DONE: Able to mutate instances.
+
+class _TagBase:
+
+    def __getitem__(self, body):
+        '''Return self, mutated by self.body = body.'''
+
+        self.body = body
+        return self
+
+    def __init__(*argv, **kwargs):
+
+        self = argv[0]          # In trouble if this fails.
+
+        if len(argv) != 1:
+            # TODO: Revise and doctest this.
+            name = type(self).__name__
+            # TypeError: f() takes exactly 0 arguments (1 given)
+            msg = '{0}() does not take positional arguments ({1} given)'
+            raise TypeError(msg.format(name, len(argv) - 1))
+
+
+class tagtype(type):
+    '''
+    >>> tag = tagtype('tag', (), {})
+    >>> a = tag(aaa='1', bbb='2')
+    >>> b = a[1, 2, 3]
+    >>> c = tag[1, 2, 3]
+    >>> tag == type(a) == type(b) == type(c)
+    True
+    >>> a == b
+    True
+
+    >>> type(tag) == tagtype
+    True
+    '''
+
+    # TODO: This is a basic class modifier - manipulate type and
+    # bases.
+    def __new__(cls_type, name, bases, cls_dict):
+
+        bases = bases + (_TagBase,)
+        return type.__new__(cls_type, name, bases, cls_dict)
+
+
+    def __getitem__(self, body):
+        '''Returns self()[body], to permit elt[...].
+        '''
+        return self()[body]
 
 
 if __name__ == '__main__':
